dd89eca3c378519a407d27afcd160f8d08877a47
Hoshino Tsuki Mio <niisanloli6@gmail.com>
2024-07-28 07:47:14 +0700

    Delete OwO directory

--- OwO/OwO Console/! Update Library.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Activating virtual environment...
-call !venv\Scripts\activate.bat
-echo Installing requirements...
-python -m pip install -r lib.lib
-echo Done!
--- OwO/OwO Console/README.txt
+++
@@ -1,3 +0,0 @@
-step 1 run file ! Create venv and Install lib .bat  to install venv and install library
-edit data.json in folder data
-to run bot you can use run_bot.bat now 
