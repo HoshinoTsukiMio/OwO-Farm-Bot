ac32dc08cb3b5af79e465ee45b587f6366065f91
Hoshino Tsuki Mio <niisanloli6@gmail.com>
2024-07-28 07:47:51 +0700

    Add files via upload

---
+++ OwO Console/! Update Bot.bat
@@ -0,0 +1,3 @@
+call !venv\Scripts\activate.bat
+python core\!Update.py
+pause
---
+++ OwO Console/! Update Library.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Activating virtual environment...
+call !venv\Scripts\activate.bat
+echo Installing requirements...
+python -m pip install -r lib.lib
+echo Done!
