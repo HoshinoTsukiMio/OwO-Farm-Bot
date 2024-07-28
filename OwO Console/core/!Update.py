ec6d8ce759b0a45db5c409f9affb51beb0d9e686
Hoshino Tsuki Mio <niisanloli6@gmail.com>
2024-07-28 12:47:40 +0700

    Update !Update.py

--- OwO Console/core/!Update.py
+++ OwO Console/core/!Update.py
@@ -2,10 +2,10 @@ import requests, os, json, time
 current_dir = os.getcwd()
 
 # Join the current directory with the filename
-flie_data = os.path.join(current_dir, 'core\\main.py')
+flie_data = os.path.join(current_dir, 'core\\!Main.py')
 file_config = os.path.join(current_dir, 'data\\setting_config.json')
 print("Updating...")
-update = requests.get("https://raw.githubusercontent.com/HoshinoTsukiMio/OwO-farm-bot/Main/owo_console/core/Main.py")
+update = requests.get("https://raw.githubusercontent.com/HoshinoTsukiMio/OwO-Farm-Bot/main/OwO%20Console/core/!Main.py")
 with open(flie_data, "wb") as f:
     f.write(update.content)
     f.close()
