5e03b73e2a86284280d537cb19428d756316b3f3
Hoshino Tsuki Mio <niisanloli6@gmail.com>
2024-08-18 20:53:12 +0700

    Update !Update.py

--- OwO Console/core/!Update.py
+++ OwO Console/core/!Update.py
@@ -1,10 +1,26 @@
 import requests, os
+
 current_dir = os.getcwd()
 
 # Join the current directory with the filename
-flie_data = os.path.join(current_dir, 'core\\!Main.py')
+flie_data = os.path.join(current_dir, "core\\!Main.py")
+versions_file = os.path.join(current_dir, "data\\version")
 print("Updating...")
-update = requests.get("https://raw.githubusercontent.com/HoshinoTsukiMio/OwO-Farm-Bot/main/OwO%20Console/core/!Main.py")
+
+update = requests.get(
+    "https://raw.githubusercontent.com/HoshinoTsukiMio/OwO-Farm-Bot/main/OwO%20Console/core/!Main.py"
+)
+version = requests.get(
+    "https://raw.githubusercontent.com/HoshinoTsukiMio/OwO-Farm-Bot/main/OwO%20Console/data/version"
+)
+versions = version.text.strip()
 with open(flie_data, "wb") as f:
     f.write(update.content)
     f.close()
+with open(versions_file, "rb") as fs:
+    version = fs.read()
+    fs.close()
+if versions != version:
+    with open(flie_data, "wb") as ff:
+        ff.write(versions)
+        ff.close()
